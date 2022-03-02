50d474773829c15a41461bce6bd21bd34e313eca
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-03-02 15:19:02 -0800

    Update bump-version.py

--- .github/bump-version.py
+++ .github/bump-version.py
@@ -1,71 +1,118 @@
-name: VERSION BUMP AUTOMATION
+import os
+import sys
+import time
+import shutil
+import yaml
+import git
+import json
+from json.decoder import JSONDecodeError
+import operator
+from stat import ST_CTIME
 
-on:
-  pull_request:
-    branches: [master, develop]
-#     types: [closed]
 
-jobs:
-  compute-version:
-    #     if: ${{ github.event.pull_request.merged }}
-    runs-on: ubuntu-latest
-    outputs:
-      matrix: ${{ steps.semver.outputs.matrix }}
-      branch_name: ${{ steps.branch_name.outputs.branch_name }}
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v2
+def get_remote():
+    username = "vivian-fan"
+    password = sys.argv[3]
+    remote = f"https://{username}:{password}@github.com/vivian-fan/version-bump-poc.git"
+    return remote
 
-      - name: Install Python3
-        uses: actions/setup-python@v2
-        with:
-          python-version: "3.x"
 
-      - name: Install Dependencies
-        run: |
-          chmod +x ./.github/intall-py-dependecies.sh
-          ./.github/intall-py-dependecies.sh
+def get_clone_repo(remote, path, branch):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    clone_repo = git.Repo.clone_from(remote, path, branch=branch)
+    return clone_repo
 
-      - name: Run Computer Version Script
-        env:
-          USER_PASSWORD: ${{ secrets.TOKEN }}
-        id: semver
-        run: |
-          python ./.github/bump-version.py $GITHUB_HEAD_REF $GITHUB_BASE_REF $USER_PASSWORD
-          echo "::set-output name=matrix::$(python ./.github/bump-version.py $GITHUB_HEAD_REF $GITHUB_BASE_REF $USER_PASSWORD)"
 
-      - name: Get Branch Reference
-        id: branch_name
-        run: echo "::set-output name=branch_name::$GITHUB_HEAD_REF"
+def read_intents(path, intent_file):
+    with open(path + "/" + intent_file, "r") as intent_content:
+        return yaml.safe_load(intent_content)
 
-  update-yaml:
-    needs: compute-version
-    runs-on: ubuntu-latest
-    strategy:
-      matrix: ${{fromJSON(needs.compute-version.outputs.matrix)}}
-      fail-fast: true
-      max-parallel: 1
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v2
 
-      - name: Update Version on Yaml File
-        uses: fjogeleit/yaml-update-action@master
-        with:
-          valueFile: ${{ matrix.file }}
-          propertyPath: info.version
-          value: ${{ matrix.version }}
-          commitChange: true
-          updateFile: true
-          targetBranch: ${{ needs.compute-version.outputs.branch_name }}
-          masterBranchName: master
-          createPR: false
-          branch: ${{ needs.compute-version.outputs.branch_name }}
-          message: Bump version to ${{ matrix.version }} in ${{ matrix.file }}
+def get_version(path, file):
+    with open(path + "/" + file, "r") as spec_content:
+        spec_content = yaml.safe_load(spec_content)
+    return spec_content["info"]["version"]
 
-  finish-workflow:
-    needs: update-yaml
-    runs-on: ubuntu-latest
-    steps:
-      - name: Report
-        run: echo "Workflow is finished"
+
+def minor_bump(version):
+    major, minor, patch = version.split(".")
+    return major + "." + str(int(minor) + 1) + "." + patch
+
+
+def major_bump(version):
+    major, minor, patch = version.split(".")
+    return str(int(major) + 1) + "." + "0" + "." + patch
+
+
+def is_less_than(version1, version2):
+    version1 = version1.replace(".", "")
+    version2 = version2.replace(".", "")
+    return version1 < version2
+
+
+def compute_next_version(intent, latest_release_version, target_branch_version):
+    next_version = None
+    if intent == "minor":
+        next_version = minor_bump(latest_release_version)
+    else:
+        next_version = major_bump(latest_release_version)
+    if is_less_than(next_version, target_branch_version):
+        next_version = target_branch_version
+    return next_version
+
+def mostRecentIntentFile(path):
+    all_files = os.listdir(path);
+    file_times = dict();
+    for file in all_files:
+        if file.endswith("intent.yml"):
+            file_times[file] = time.time() - os.stat(path + "/" + file).st_ctime;
+    return  sorted(file_times.items(), key=operator.itemgetter(1))[0][0]
+
+feature_branch = str(sys.argv[1])
+target_branch = str(sys.argv[2])
+
+release_path = "./release"
+target_path = "./" + target_branch
+feature_path = "./" + feature_branch
+
+remote = get_remote()
+
+clone_repo_target = get_clone_repo(remote, target_path, target_branch)
+clone_repo_feature = get_clone_repo(remote, feature_path, feature_branch)
+
+release_branches = []
+for branch in clone_repo_target.refs:
+    if branch.__str__().startswith("origin/production_release"):
+        release_branches.append(branch.__str__())
+release_branches.sort()
+latest_release_branch = release_branches[-1].replace("origin/", "")
+
+clone_repo_release = get_clone_repo(remote, release_path, latest_release_branch)
+
+next_version_list = {"include": []}
+
+recent_intent_file = mostRecentIntentFile(feature_path + "/")
+# for commit in clone_repo_feature.iter_commits(feature_branch):
+#     for file in commit.stats.files:
+#         if file.endswith("intent.yml"):
+#             newly_committed_intent_file = file
+#             print('debug: ', commit, file)
+
+intents = read_intents(feature_path, recent_intent_file)
+
+for file_name, intent in intents["intent"].items():
+    file = file_name + ".yaml"
+    latest_release_version = get_version(release_path, file)
+    target_branch_version = get_version(target_path, file)
+    next_version = compute_next_version(
+        intent, latest_release_version, target_branch_version
+    )
+    next_version_list["include"].append({"file": file, "version": next_version})
+
+shutil.rmtree(release_path)
+shutil.rmtree(target_path)
+shutil.rmtree(feature_path)
+
+print(json.dumps(next_version_list))
